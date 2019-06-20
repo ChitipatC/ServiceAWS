3b96d23801b97541c2c7d5f9ff5f78dbe8e0dc9d
Chitipat <chitipat.cha@gmail.com>
2019-06-20 17:49:46 +0700

    Updated response

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -1,13 +1,13 @@
 package com.aws.codestar.projecttemplates.handler;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-
-import com.aws.codestar.projecttemplates.GatewayResponse;
-import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.aws.codestar.projecttemplates.GatewayResponse;
 
 /**
  * Handler for requests to Lambda function.
@@ -17,6 +17,6 @@ public class HelloWorldHandler implements RequestHandler<Object, Object> {
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        return new GatewayResponse(new JSONObject().put("Output", "Hello World!"+input.toString()).toString(), headers, 200);
+        return new GatewayResponse(new JSONObject().put("Output", input.toString()).toString(), headers, 200);
     }
 }
