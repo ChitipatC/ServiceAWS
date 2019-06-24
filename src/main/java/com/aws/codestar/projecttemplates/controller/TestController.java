66e2c504ba5d52738dbab0bf86b676ca274cebe3
Chitipat <chitipat.cha@gmail.com>
2019-06-24 11:43:23 +0700

    Edited TestController

--- src/main/java/com/aws/codestar/projecttemplates/controller/TestController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/TestController.java
@@ -1,28 +1,22 @@
 package com.aws.codestar.projecttemplates.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.json.JSONObject;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.aws.codestar.projecttemplates.GatewayResponse;
 
 public class TestController implements RequestHandler<Object, Object> {
 
 	@Override
 	public Object handleRequest(final Object input, final Context context) {
-		if(input==null) {
-			return "Response : null!";
+		if(input==null||input=="") {
+			return "====Body : null!====";
 		}
-		return getResponse(input);
+		return getResponse(input,context);
 	}
 	
-	public Object getResponse(Object input) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        return new GatewayResponse(new JSONObject().put("Output", input.toString()).toString(), headers, 200);
+	public Object getResponse(Object input,Context context) {
+    	context.getLogger().log("=====In handleRequest=====");
+    	
+        return "=====RESPONES : "+input.toString()+"=====";
 	}
 
 }
--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -1,16 +1,7 @@
 package com.aws.codestar.projecttemplates.handler;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.json.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.aws.codestar.projecttemplates.GatewayResponse;
 
 /**
  * Handler for requests to Lambda function.
@@ -18,8 +9,7 @@ import com.aws.codestar.projecttemplates.GatewayResponse;
 public class HelloWorldHandler implements RequestHandler<Object, Object> {
 	
     public Object handleRequest(final Object input, final Context context) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        return new GatewayResponse(new JSONObject().put("Output", input.toString()).toString(), headers, 200);
+    	context.getLogger().log("In handleRequest");
+        return input.toString();
     }
 }
